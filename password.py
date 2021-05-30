7ee39140b22bc0fcac14571a7af86c825dce325f
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-30 14:35:15 +0300

    Update the changes done

---
+++ password.py
@@ -0,0 +1,15 @@
+class Password:
+     def __init__(self, username, email, phone_number):
+         
+          """
+          username: New password username.
+          email: New password email.
+          number: New password number.
+          """
+          self.username = username
+          self.email = email
+          self.phone_number = number
+ 
+         
+new_password = Password( )
+print(new_password)
