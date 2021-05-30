ed39ba0c75f8d9d765ad8a7b963dae6067bdee81
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-30 16:41:23 +0300

    Comment on the second test

--- password_test.py
+++ password_test.py
@@ -11,6 +11,7 @@ class TestPassword(unittest.TestCase):
             self.assertEqual(self.new_password.email,"melbyokozi11@gmail.com")
             self.assertEqual(self.new_password,phone_number,"0718818642")
             
+    #Test 2
     def test_save_password(self): 
         self.new_password.save_password() 
         self.assertEqual(len(Password.password_list),1)
