63972b8fac669e618a8c7d1c2d224fc4424dbed5
Melby01 <melby.okozi@student.moringaschool.com>
2021-05-30 17:01:00 +0300

    Create a delete test from the password list

--- password.py
+++ password.py
@@ -1,21 +1,25 @@
-class Password: 
-    pass 
-  #Class that generates new instances of passwords.
-  
-password_list = []
+class Password:
+    pass
+    # Class that generates new instances of passwords.
+    password_list = []
+
+
 def save_password(self):
-      Password.password_list.append(self)
-  
-def __init__(self, username, email, phone_number):
-          
-         # username: New password username.
-         # email: New password email.
-         # number: New password number.
-           
-          self.username = username
-          self.email = email
-          self.phone_number = phone_number
- 
-         
-new_password = Password("Melby01", "melbyokozi11@gmail.com", "0718818642")
+    Password.password_list.append(self)
+
+
+def __init__(self, username, email, phone_number, password):
+
+    # username: New password username.
+    # email: New password email.
+    # number: New password number.
+    #password: New password.
+
+    self.username = username
+    self.email = email
+    self.phone_number = phone_number
+    self.password = password
+
+
+new_password = Password("Melby01", "melbyokozi11@gmail.com", "0718818642", "Orinah8644")
 print(new_password)
--- password_test.py
+++ password_test.py
@@ -10,6 +10,7 @@ class TestPassword(unittest.TestCase):
             self.assertEqual(self.new_password.username,"Melby01")
             self.assertEqual(self.new_password.email,"melbyokozi11@gmail.com")
             self.assertEqual(self.new_password,phone_number,"0718818642")
+            self.assertEqual(self.new_password,password,"Orinah8644")
             
     #Test 2
     def test_save_password(self): 
@@ -19,11 +20,18 @@ class TestPassword(unittest.TestCase):
     #Test 3
     def test_save_multiple_password(self):
         self.new_password.save_password()
-        test_password = Password("Test","Okozi01","okozimelby11@gmail.com","0798118444")
+        test_password = Password("Test","Okozi01","okozimelby11@gmail.com","0798118444","Orinah8644")
         test_password.save_password()
         self.assertEqual(len(Password.password_list),2)
         
-    def
+    def tearDown(self):
+        Password.password_list = []
+    def test_save_multiple_password(self):
+        self.new_password.save_password()
+        test_contact = Password("Test","Okozi01","okozimelby11@gmail.com","0798118444","Orina8644")
+        
+    def test_delete_password(self):
+        
            
 if __name__ == '__main__':
     unittest.main()
